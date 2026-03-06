672f609bb9c0e6920c0f34818f5a5c46da4c3ba9
Heidy <22041154@itdurango.edu.mx>
2026-03-06 14:03:50 -0600

    Remover carpeta de API del repo de UI

--- censo-tecnologico/src/tipos-computadoras/tipos-computadoras.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TiposComputadorasController } from './tipos-computadoras.controller';
-
-@Module({
-  controllers: [TiposComputadorasController]
-})
-export class TiposComputadorasModule {}
